a5b89a029e9a9074aca636849e659281b4d65ff9
Ajish-TM <ajish.tm427@gmail.com>
2024-10-26 21:59:39 +0530

    add curd

--- app/controllers/admin/services_controller.rb
+++ app/controllers/admin/services_controller.rb
@@ -1,62 +1,66 @@
 class Admin::ServicesController < ApplicationController
-    before_action :authenticate_user!
-    before_action :ensure_admin
-    before_action :set_service, only: [:show, :edit, :update, :destroy]
-  
-    def index
-      @services = Service.all
-    end
-  
-    def new
-      @service = Service.new
-    end
-  
-    def create
-      @service = Service.new(service_params)
-  
-      if @service.save
-        flash[:notice] = "Service created successfully!"
-        redirect_to admin_services_path
-      else
-        flash[:alert] = "Failed to create service."
-        render :new
-      end
-    end
-  
-    def edit
-    end
-  
-    def update
-      if @service.update(service_params)
-        flash[:notice] = "Service updated successfully!"
-        redirect_to admin_services_path
-      else
-        flash[:alert] = "Failed to update service."
-        render :edit
-      end
-    end
-  
-    def destroy
-      @service.destroy
-      flash[:notice] = "Service deleted successfully!"
-      redirect_to admin_services_path
-    end
-  
-    private
-  
-    def set_service
-      @service = Service.find(params[:id])
-    end
-  
-    def service_params
-      params.require(:service).permit(:name, :description, :duration, :price)
-    end
-  
-    def ensure_admin
-      unless current_user.admin?
-        flash[:alert] = 'You are not authorized to perform this action.'
-        redirect_to root_path
-      end
+  before_action :authenticate_user!
+  before_action :ensure_admin
+  before_action :set_service, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @services = Service.all
+  end
+
+  def show
+    @service = Service.find(params[:id])
+    # This action will render the show view for a specific service
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+
+    if @service.save
+      flash[:notice] = "Service created successfully!"
+      redirect_to admin_services_path # Redirecting to the index action
+    else
+      flash[:alert] = "Failed to create service."
+      render :new
     end
   end
-  
+
+  def edit; end
+
+  def update
+    if @service.update(service_params)
+      flash[:notice] = "Service updated successfully!"
+      redirect_to admin_services_path # Redirecting to the index action
+    else
+      flash[:alert] = "Failed to update service."
+      render :edit
+    end
+  end
+
+  def destroy
+    @service = Service.find(params[:id]) 
+    @service.destroy
+    flash[:notice] = "Service deleted successfully!"
+    redirect_to admin_services_path # Redirecting to the index action
+  end
+
+  private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(:name, :description, :duration, :price)
+  end
+
+  def ensure_admin
+    unless current_user.admin?
+      flash[:alert] = 'You are not authorized to perform this action.'
+      redirect_to root_path
+    end
+  end
+end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 import "@hotwired/turbo-rails"
 import "@hotwired/stimulus"
 import "@hotwired/stimulus-loading"
-// Include Rails UJS if you have it installed as a separate package
-import Rails from "@rails/ujs"
-Rails.start()
+import Rails from '@rails/ujs';
+Rails.start();
